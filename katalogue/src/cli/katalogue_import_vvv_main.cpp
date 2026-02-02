1ce828c58f7360d687612e167baf2851ed1e68b5
infinityabundance <255699974+infinityabundance@users.noreply.github.com>
2026-02-02 17:13:25 +0000

    Phase 18: pre-release polish and versioning

--- katalogue/src/cli/katalogue_import_vvv_main.cpp
+++ katalogue/src/cli/katalogue_import_vvv_main.cpp
@@ -1,8 +1,16 @@
 #include <QCoreApplication>
 #include <QTextStream>
 
+#include "katalogue_version.h"
+
 int main(int argc, char **argv) {
     QCoreApplication app(argc, argv);
+    const QStringList args = app.arguments();
+    if (args.contains(QStringLiteral("--version"))) {
+        QTextStream out(stdout);
+        out << "Katalogue VVV import tool " << KATALOGUE_VERSION_STRING << Qt::endl;
+        return 0;
+    }
     QTextStream out(stderr);
     out << "katalogue-import-vvv is not implemented in this build.\n"
            "Rebuild with Firebird client integration and ENABLE_VVV_IMPORT=ON.\n";
